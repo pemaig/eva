4f02bde0d9279dfa9736507f5de9e6f76881ab44
Maxim Pestriakov <maxim.pestriakov@sirenltd.com>
2023-04-08 18:25:55 +0300

    add math operations

--- Eva.js
+++ Eva.js
@@ -1,48 +1,30 @@
 const assert = require('assert');
 
 const UNIMPLEMENTED_ERROR = 'Unimplemented expression: ';
-const ADDITION_ERROR = 'One of the addition variables is not a Number';
 
-function _throw(msg) {
-    throw new Error(msg);
-}
-function isNumber(exp) {
-    return typeof exp === 'number';
-}
-function isString(exp) {
-    return (
-        typeof exp === 'string' &&
-        exp[0] === '"' &&
-        exp[exp.length - 1] === '"'
-    );
-}
-function isAddition(exp) {
-    return exp[0] === '+';
-}
-function sum(exp) {
-    const left = exp[1];
-    const right = exp[2];
+const _throw = (msg) => { throw new Error(msg) };
+const isNumber = (exp) => typeof exp === 'number';
+const isString = (exp) => (
+    typeof exp === 'string' &&
+    exp[0] === '"' &&
+    exp[exp.length - 1] === '"'
+);
+const isAddition = (exp) => exp[0] === '+';
+const isSubstracrion = (exp) => exp[0] === '-';
+const isMultiplication = (exp) => exp[0] === '*';
+const isDivision = (exp) => exp[0] === '/';
+const isRemainder = (exp) => exp[0] === '%';
 
-    return (
-        (isAddition(left) ? sum(left)
-            : isNumber(left) ? left
-                : _throw(ADDITION_ERROR)
-        )
-        +
-        (isAddition(right) ? sum(right)
-            : isNumber(right) ? right
-                : _throw(ADDITION_ERROR)
-        )
-    );
-}
 
-// todo: add -,*,/,%
 class Eva {
     eval(exp) {
         if (isNumber(exp)) return exp;
         if (isString(exp)) return exp.slice(1, -1);
-        // if (isAddition(exp)) return sum(exp);
         if (isAddition(exp)) return this.eval(exp[1]) + this.eval(exp[2]);
+        if (isSubstracrion(exp)) return this.eval(exp[1]) - this.eval(exp[2]);
+        if (isMultiplication(exp)) return this.eval(exp[1]) * this.eval(exp[2]);
+        if (isDivision(exp)) return this.eval(exp[1]) / this.eval(exp[2]);
+        if (isRemainder(exp)) return this.eval(exp[1]) % this.eval(exp[2]);
 
         throw (UNIMPLEMENTED_ERROR + JSON.stringify(exp));
     }
@@ -53,6 +35,13 @@ const eva = new Eva();
 assert.strictEqual(eva.eval(1), 1);
 assert.strictEqual(eva.eval('"hello"'), 'hello');
 assert.strictEqual(eva.eval(['+', 1, 5]), 6);
+assert.strictEqual(eva.eval(['-', 5, 1]), 4);
+assert.strictEqual(eva.eval(['*', 2, 3]), 6);
+assert.strictEqual(eva.eval(['/', 8, 4]), 2);
+assert.strictEqual(eva.eval(['%', 3, 2]), 1);
 assert.strictEqual(eva.eval(['+', ['+', 2, 3], 5]), 10);
-assert.strictEqual(eva.eval(true), 1);
+assert.strictEqual(eva.eval(['-', ['-', 3, 2], 1]), 0);
+assert.strictEqual(eva.eval(['*', ['*', 3, 2], 2]), 12);
+assert.strictEqual(eva.eval(['/', ['/', 6, 2], 3]), 1);
+assert.strictEqual(eva.eval(['%', ['%', 9, 5], 3]), 1);
 console.log('Tests are passed');
